414cdc96926862a54427048638f9eb41542fe605
gasstin <gastonl1994@gmail.com>
2022-06-06 08:36:37 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts: print a string
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);
 	return (0);
 }
