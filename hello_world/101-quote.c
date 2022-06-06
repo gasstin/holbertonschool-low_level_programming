ab02cc62ba98c3b61da5a001af4a039728abef39
gasstin <gastonl1994@gmail.com>
2022-06-06 11:22:09 -0700

    task 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts: print a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
+
+	return (1);
+}
