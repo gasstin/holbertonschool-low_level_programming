1a5cba9281d0921fd975165cebb3f583c77a17a4
gasstin <gastonl1994@gmail.com>
2022-06-07 05:17:52 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int l;
+
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
