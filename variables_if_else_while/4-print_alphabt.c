264377e0f5423e18ba852c05e01b6112a51d5b32
gasstin <gastonl1994@gmail.com>
2022-06-07 05:07:30 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int l;
+
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'e' && l != 'q')
+		{
+		putchar(l);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
