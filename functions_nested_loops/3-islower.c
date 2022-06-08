8f608d9f60466ebba923352a411327f77fff7694
gasstin <gastonl1994@gmail.com>
2022-06-08 14:15:43 -0700

    task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,13 +8,12 @@
  */
 int _islower(int c)
 {
-	if ((c >= 61) && (c <= 122))
+	if ((c >= 97) && (c <= 122))
 	{
-		_putchar('1');
+		return (1);
 	}
 	else
 	{
-		_putchar('0');
+		return (0);
 	}
-	return (0);
 }
