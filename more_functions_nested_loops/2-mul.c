3c7c6eabe0632b57f963591ab4200a32f4ce824d
gasstin <gastonl1994@gmail.com>
2022-06-09 11:43:12 -0700

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+ * mul - prints the add of l + m
+ * @a: is the firts integer
+ * @b: is the second integer
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
