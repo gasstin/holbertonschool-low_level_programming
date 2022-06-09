6d51352e40d56b37ee5f346312fe587ecc3f00ef
gasstin <gastonl1994@gmail.com>
2022-06-09 12:05:43 -0700

    task 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar ('\n');
+}
