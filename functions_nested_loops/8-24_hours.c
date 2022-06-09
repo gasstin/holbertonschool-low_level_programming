a30ea4360820cc7910bf3c44f4832643744c1d9e
gasstin <gastonl1994@gmail.com>
2022-06-09 06:44:37 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * jack_bauer - prints the clock 24 hs
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 23; n++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar ((n / 10) + '0');
+			_putchar ((n % 10) + '0');
+			_putchar (58);
+			_putchar ((m / 10) + '0');
+                        _putchar ((m % 10) + '0');
+			_putchar ('\n');
+		}
+		}
+}
