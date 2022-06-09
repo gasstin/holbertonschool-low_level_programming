3bd8ebc2594d3133895d5a39f59e0b43c0dcba84
gasstin <gastonl1994@gmail.com>
2022-06-09 06:27:44 -0700

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+# include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int n;
+	int m;
+	int o;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+		o = m * n;
+		if (m != 0)
+		{
+			_putchar (44);
+			_putchar (32);
+		}
+		if (o < 10)
+                {
+                	if (m != 0)
+			{
+			_putchar (32);
+			}
+			_putchar (o + '0');
+                }
+                else
+                {
+                        _putchar ((o / 10) + '0');
+                        _putchar ((o % 10) + '0');
+                }
+		}
+		_putchar ('\n');
+	}
+}
