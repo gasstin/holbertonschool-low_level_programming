e343b5fffccae0ff13d317b8be34dd299019ef72
gasstin <gastonl1994@gmail.com>
2022-06-09 06:51:36 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+ * add - prints the add of l + m
+ * @l: is the firts integer
+ * @m: is the second integer
+ *
+ * Return: Always 0.
+ */
+int add(int l, int m)
+{
+	return (l + m);
+	_putchar ('\n');
+}
