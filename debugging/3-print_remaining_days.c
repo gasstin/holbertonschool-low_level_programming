c01a2368d916c252cd95dd67aad85534f8d305c4
gasstin <gastonl1994@gmail.com>
2022-06-10 12:42:34 -0700

    task 3

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -20,7 +20,14 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
+	if (year == 2000)
+	{
+		printf("Remaining days: %d\n", 365 - day);
+	}
+	else
+	{
+		printf("Remaining days: %d\n", 366 - day);
+	}
 	}
 	else
 	{
