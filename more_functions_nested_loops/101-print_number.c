a8dc8d5fd180c6b8a26e058d5d048713c80bb722
gasstin <gastonl1994@gmail.com>
2022-06-13 07:18:24 -0700

    task 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -21,9 +21,9 @@ void print_number(int n)
 			_putchar (-n % 10 + '0');
 		}
 	}
-	else if
+	else if ( n == 0)
 	{
-		_putchar('0');
+		_putchar ('0');
 	}
 	else if ((n / 10) == 0 && (n % 10 != 0) && (n >= 0))
 	{
