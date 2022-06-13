fdf79ee6049614f33ab656f3c8b43bb1a2f76bea
gasstin <gastonl1994@gmail.com>
2022-06-13 07:16:33 -0700

    task 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 # include "main.h"
 
 /**
- * print_number - prints numbers from 0 to 9 less 2 and 4
+ * print_number - prints the integer n
  * @n: is a variable
  *
  * Return: Always 0.
@@ -21,6 +21,10 @@ void print_number(int n)
 			_putchar (-n % 10 + '0');
 		}
 	}
+	else if
+	{
+		_putchar('0');
+	}
 	else if ((n / 10) == 0 && (n % 10 != 0) && (n >= 0))
 	{
 		_putchar (n % 10 + '0');
@@ -29,5 +33,5 @@ void print_number(int n)
 	{
 		_putchar (45);
 		_putchar (-n % 10 + '0');
-	}	
+	}
 }
