37a17fa8753bfc37a2b50f1b20c3ab57e4837151
gasstin <gastonl1994@gmail.com>
2022-06-14 10:09:27 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * print_rev - Write a function that prints a string,
+ * in reverse, followed by a new line, to stdout.
+ * @str: is the string
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar (str[i]);
+		i--;
+	}
+
+	_putchar ('\n');
+}
