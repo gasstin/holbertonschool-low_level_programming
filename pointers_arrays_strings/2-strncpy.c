db53e55709382275c001f576efda13409ffa6ccc
gasstin <gastonl1994@gmail.com>
2022-06-15 14:58:00 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * _strncpy - Write a function that copy a strings.
+ * @dest: is the string of destination
+ * @src: is the string of source
+ * @n: is the number of bytes.
+ *
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (src[i] != '\0')
+		i++;
+	while (j != n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	return (dest);
+}
