4535f75c45184fba2db6df55eb5620fa6647b0b4
gasstin <gastonl1994@gmail.com>
2022-06-16 11:12:21 -0700

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * cap_string - Write a function that capitalizes all words of a string.
+ * @a: is the string.
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	while (*(a + i))
+	{
+		if (a[i] == 32 || a[i] == 9 || a[i] == 10 || a[i] == 44)
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z' && a[i + 1] != '\0')
+				a[i + 1]  = a[i + 1] - 32;
+		}
+		else if (a[i] == 59 || a[i] == 46 || a[i] == 33 || a[i] == 63)
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z' && a[i + 1] != '\0')
+				a[i + 1]  = a[i + 1] - 32;
+		}
+		else if (a[i] == 34 || a[i] == 40 || a[i] == 41)
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z' && a[i + 1] != '\0')
+				a[i + 1]  = a[i + 1] - 32;
+		}
+		else if (a[i] == 123 || a[i] == 125)
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z' && a[i + 1] != '\0')
+				a[i + 1]  = a[i + 1] - 32;
+		}
+	i++;
+	}
+	return (a);
+}
