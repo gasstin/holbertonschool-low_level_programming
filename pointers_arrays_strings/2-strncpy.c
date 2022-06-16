b9e1f89d867436be4534a9482c72a38a12f54863
gasstin <gastonl1994@gmail.com>
2022-06-16 05:46:48 -0700

    task 2}

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,15 +15,18 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (src[i] != '\0')
-		i++;
-	while (j != n)
+	if (src != NULL)
 	{
-			if (src[j] != '\0')
+		while (src[i] != '\0')
+			i++;
+		while (j != n)
+		{
+			if (j <= i)
 				dest[j] = src[j];
 			else
 				dest[j] = '\0';
 			j++;
+		}
 	}
 	return (dest);
 }
