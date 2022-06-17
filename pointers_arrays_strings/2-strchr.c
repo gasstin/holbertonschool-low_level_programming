3b4904585be7430e8d5660fcb7c0db0d04ae7077
gasstin <gastonl1994@gmail.com>
2022-06-17 06:36:45 -0700

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * _strchr - Write a function that locates a character in a string.
+ * @s: is the string to find
+ * @c: is the caracter
+ *
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		s++;
+	}
+	return (s);
+}
