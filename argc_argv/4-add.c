4fd8e5f3d3b050c857e3605581b19691bd7b698d
gasstin <105363918+gasstin@users.noreply.github.com>
2022-06-21 19:07:35 -0300

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -23,10 +23,13 @@ int main(int argc, char *argv[])
 			{
 				if (argv[n][m] > 57 || argv[n][m] < 48)
 				{
-					return (1);
 					printf("Error\n");
 					break;
 				}
+			}
+			if (argv[n][m] > 57 || argv[n][m] < 48)
+			{
+			return (1);
 			}
 			add += atoi(argv[n]);
 		}
