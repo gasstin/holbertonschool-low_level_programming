881809ae1bb10d7ba15822cf641dd5eb25f47c46
gasstin <gastonl1994@gmail.com>
2022-06-21 06:09:18 -0700

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
- * mynameis- Write a function that prints its name
- * @dest: is the string of destination
- * @src: is the string of source
- * @n: is the number of bytes
+ * main- Write a function that prints its name
+ * @argc: is the size of the command line
+ * @argv: is the pointer array to the command line arguments
  *
  * Return: Always 0.
  */
@@ -13,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 
 	return (0);
 }
