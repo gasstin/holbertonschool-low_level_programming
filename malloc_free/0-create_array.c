90a28258ebd184b92f1983762c86196d415d9629
gasstin <gastonl1994@gmail.com>
2022-06-22 13:13:13 -0700

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,13 +15,13 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
+	if (p == NULL)
+		return (p);
 	if (size)
 	{
 		p = malloc(sizeof(char) * size);
 		for (i = 0; i < size; i++)
 			p[i] = c;
-		if (*p == NULL)
-			return (NULL);
 	}
 	else
 		p = NULL;
