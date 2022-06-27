82329b25e8bd30cb322ea8b5e030d40ef4f2d2c2
gasstin <gastonl1994@gmail.com>
2022-06-27 12:18:22 -0700

    task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Write a function that allocates memory
+ * @b: are the bytes to pass to malloc
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
