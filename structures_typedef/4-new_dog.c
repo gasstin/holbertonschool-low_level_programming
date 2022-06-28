85aaba41dddef043afe706e1f2ca31ff87d2e80d
gasstin <gastonl1994@gmail.com>
2022-06-28 06:34:58 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,14 +14,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new = NULL;
+	char *new_name = NULL;
+	char *new_owner = NULL;
 
 	new = malloc(sizeof(dog_t));
 	if (new)
 	{
-		new->name = name;
+		new_name = malloc(sizeof(name));
+		if (new_name)
+			new_name = name;
+		else
+		{
+			free(new_name);
+			free(new);
+		}
+		new->name = new_name;
 		new->age = age;
-		new->owner = owner;
+		new_owner = malloc(sizeof(owner));
+		if (new_owner)
+			new_owner = owner;
+		else
+		{
+			free(new_owner);
+			free(new);
+		}
+		new->owner = new_owner;
 	}
+	else
+		free(new);
 
 	return (new);
 }
