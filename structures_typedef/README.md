b9c3bb3ef3360208bd5e10daa444278bf7f92f18
gasstin <gastonl1994@gmail.com>
2022-06-28 04:19:19 -0700

    task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+I'll learn about structures and typedef projects
---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - structure for a dog propieties
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner's dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
