3b1b867d7e4b0890393275d333d78fe2584370b1
gasstin <gastonl1994@gmail.com>
2022-06-29 10:00:34 -0700

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,16 +22,12 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i = 0;
-
-	if (s)
+ 
+	while (*ops[i].op)
 	{
-		while (*ops[i].op)
-		{
-			if (*ops[i].op == *s)
-				return (*ops[i].f);
-			else
-				i++;
-		}
+		if (*ops[i].op == *s)
+			return (*ops[i].f);
+		i++;
 	}
 
 	return (NULL);
