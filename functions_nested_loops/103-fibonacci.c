f01b5d7b49a1639af17fd5bf447f3e15cc2c4cb6
gasstin <gastonl1994@gmail.com>
2022-07-04 18:40:53 -0700

    task 15

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the first
+ * 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: the sum of all multiples
+ */
+int main(void)
+{
+	unsigned long m = 0, sig = 0, ant_I = 0, ant_II = 1, sum = 0;
+
+	while (m != 50)
+	{
+		sig = ant_I + ant_II;
+		ant_I = ant_II;
+		ant_II = sig;
+		sum += sig;
+		m++;
+		if (sum >= 4000000)
+			break;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
