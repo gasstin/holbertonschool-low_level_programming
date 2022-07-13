6d33a3512a0d401f01b8940c2cf9f46845357522
gasstin <gastonl1994@gmail.com>
2022-07-13 11:09:12 -0700

    task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *paux = NULL;
 	listint_t *end = *head;
 
-	paux = malloc(sizeof(list_t));
+	paux = malloc(sizeof(listint_t));
 	if (!paux)
 		return (NULL);
 	if (end)
