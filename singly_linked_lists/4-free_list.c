e7f7a55e2e05b0eaeb2af075ab6935612ec861c3
gasstin <gastonl1994@gmail.com>
2022-07-13 08:15:01 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Write a function that frees a list_t list.
+ * @head: is the new head of the list.
+ *
+ * Return: the address of the new element, or NULL if failed
+ */
+void free_list(list_t *head)
+{
+	list_t *paux = NULL;	
+
+	while(head)
+	{
+		paux = head;
+		head = head->next;
+		free(paux->str);
+		free(paux);
+	}
+}
