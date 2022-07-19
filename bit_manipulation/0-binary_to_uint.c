17c03ad4653dab48a19dfc7ebe001e9efd2ea1fa
gasstin <gastonl1994@gmail.com>
2022-07-18 20:29:36 -0700

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_ti_unit - Write a function that converts a binary number
+ * to an unsigned int.
+ * @b: is the binary.
+ *
+ * Return: an unsigned int or 0 if function fall.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int u = 0;
+
+	if (b)
+	{
+		for (u = 0; *b; b++)
+		{
+			if (*b == '1')
+				u = (u << 1) | 1;
+			else if (*b == '0')
+				u <<= 1;
+			else
+				return (0);
+		}
+		return (u);
+	}
+	return (0);
+}
