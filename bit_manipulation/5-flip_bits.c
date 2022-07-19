59ca0d71e97adcf5ec7825d2898adf7f570efe24
gasstin <gastonl1994@gmail.com>
2022-07-19 14:32:47 -0700

    task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - Write a function that returns
+ * the number of bits you would need to flip to get
+ * from one number to another.
+ * @n: is the number.
+ * @m: is the second number.
+ *
+ * Return: 1 if it worked, or -1 it fail.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int aux = 0;
+	unsigned int index = 65;
+
+	if (n != m)
+	{
+		while (index)
+		{
+			if (((n >> index) & 1) != ((m >> index) & 1))
+				aux++;
+			index--;
+		}
+	}
+	return (aux);
+}
+
