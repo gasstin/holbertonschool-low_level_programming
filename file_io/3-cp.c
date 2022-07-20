89128a7819ef4724c8dbe04a87bedfa5b67895b5
gasstin <gastonl1994@gmail.com>
2022-07-19 21:29:43 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,18 +12,18 @@
  */
 int main(int ac, char **av)
 {
-	int res, res_1;
+	int res, res_1, cl, cl_1;
 	char *buffer;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	res = open(av[1], O_RDONLY);
 	if (res == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	res_1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -34,14 +34,16 @@ int main(int ac, char **av)
 	write(res_1, buffer, 1024);
 	if (res_1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
 		exit(99);
 	}
-	close(res);
-	close(res_1);
+	cl = close(res);
+	cl_1 = close(res_1);
+	if (cl == -1 || cl_1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl);
+		exit(100);
+	}
 	free(buffer);
-	
-
-
 	return (0);
 }
