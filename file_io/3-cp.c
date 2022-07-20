d70d56d9032916c813fe613fa92a71f86790eabf
gasstin <gastonl1994@gmail.com>
2022-07-20 06:47:47 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,11 +22,6 @@ int main(int ac, char **av)
 	}
 	/*open the files*/
 	res = open(av[1], O_RDONLY);
-	if (res == -1 || bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	res_1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	buffer = malloc(1024);
 	if (!buffer)
@@ -38,6 +33,11 @@ int main(int ac, char **av)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
+	}
+	if (res == -1 || bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	/*close the files*/
 	cl = close(res);
