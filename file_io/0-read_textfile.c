f264d34f5085382d7d049519e6e58f24437fced6
gasstin <gastonl1994@gmail.com>
2022-07-19 18:04:48 -0700

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,7 +7,10 @@
  * and prints it to the POSIX standard output.
  * @filename: is the name if the file.
  * @letters: are the numbers of letters to print.
- * Return: Always 0.
+ *
+ * Return: returns the actual number of letters it could read and print.
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
