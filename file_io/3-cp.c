5dfc02769eb62362e200e710c9e79e84c0741099
gasstin <gastonl1994@gmail.com>
2022-07-20 06:41:34 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,7 +12,7 @@
  */
 int main(int ac, char **av)
 {
-	int res, res_1, cl, cl_1;
+	int res, res_1, cl, cl_1, bytes;
 	char *buffer;
 
 	if (ac != 3)
@@ -20,6 +20,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	/*open the files*/
 	res = open(av[1], O_RDONLY);
 	if (res == -1)
 	{
@@ -30,13 +31,15 @@ int main(int ac, char **av)
 	buffer = malloc(1024);
 	if (!buffer)
 		return (-1);
-	read(res, buffer, 1024);
-	write(res_1, buffer, 2875);
+	/*read the file*/
+	while ((bytes = read(res, buffer, sizeof(char))) > 0)
+		write(res_1, buffer, bytes);
 	if (res_1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+	/*close the files*/
 	cl = close(res);
 	cl_1 = close(res_1);
 	if (cl == -1 || cl_1 == -1)
