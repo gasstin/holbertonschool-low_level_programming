8bbba434db2586d95c423cdf953d76a12cba6ecd
gasstin <gastonl1994@gmail.com>
2022-07-19 19:48:56 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,9 +17,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	aux = open(filename, O_CREAT | O_RDWR);
+	aux = open(filename, O_RDWR | O_CREAT);
 	if (aux == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	write(aux, text_content, strlen(text_content));
 
 	close(aux);
