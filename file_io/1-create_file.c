067e3ebc6736370cd5b536fc85fda3cb723d80d1
gasstin <gastonl1994@gmail.com>
2022-07-19 18:30:11 -0700

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - Create a function that creates a file.
+ * @filename: is the name if the file.
+ * @text_content: is the content of the new file.
+ *
+ * Return: if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int aux;
+
+	if (!filename)
+		return (-1);
+	aux = open(filename, O_CREAT | O_RDWR);
+	if (aux == -1)
+		return (-1);
+	write(aux, text_content, strlen(text_content));
+
+	close(aux);
+
+	return (1);
+}
