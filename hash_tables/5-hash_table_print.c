5f32c2ab4d63b5590bd3f9053e2a14ddd9caf370
gasstin <gastonl1994@gmail.com>
2022-08-20 14:13:45 -0700

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -11,21 +11,21 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *p;
-	char n;
+	int n = 0;
 
+	if(!ht)
+		return;
 	printf("{");
-	if (ht)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
 			p = ht->array[i];
-			n = 0;
 			while (p)
 			{
 				if (n)
 					printf(", ");
 				printf("'%s': '%s'", p->key, p->value);
-				p = p-> next;
+				p = p->next;
 				n = 1;
 			}
 		}
