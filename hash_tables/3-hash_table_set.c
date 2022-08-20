fe86d6210b8c7cce18ff90a69e4deb600b721b4f
gasstin <gastonl1994@gmail.com>
2022-08-20 12:38:03 -0700

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dup_key = NULL, *dup_value = NULL;
 	hash_node_t *new = NULL;
 
-	if (!ht || !key || !value)
+	if (!key || !value)
 		return (0);
 	/*duplico value*/
 	dup_value = strdup(value);
